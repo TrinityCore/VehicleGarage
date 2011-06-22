12724c1dbf784027c3fe8cc8c7603216306577bc
Machiavelli <machiavelli.trinity@gmail.com>
2011-06-23 01:24:10 +0200

    Minor naming adjustment for unks

--- VehicleGarage/DBCStructures/VehicleFlags.cs
+++ VehicleGarage/DBCStructures/VehicleFlags.cs
@@ -6,15 +6,15 @@ namespace VehicleGarage.DBCStructures
         NoStrafe = 0x1,             // Sets MOVEFLAG2_NO_STRAFE
         NoJump = 0x2,               // Sets MOVEFLAG2_NO_JUMPING
         FullSpeedTurning = 0x4,     // Sets MOVEFLAG2_FULLSPEEDTURNING
-        Unk1 = 0x8,
+        Flag_0X8 = 0x8,
         AllowPitching = 0x10,       // Sets MOVEFLAG2_ALLOW_PITCHING
         FullSpeedPitching = 0x20,   // Sets MOVEFLAG2_FULLSPEEDPITCHING
         CustomPitch = 0x40,         // If set use pitchMin and pitchMax from DBC, otherwise pitchMin = -pi/2, pitchMax = pi/2 
-        Unk2 = 0x80,
-        Unk3 = 0x100,
-        Unk4 = 0x200,
+        Flag_0X80 = 0x80,
+        Flag_0X100 = 0x100,
+        Flag_0X200 = 0x200,
         AdjustAimAngle = 0x400,     // Lua_IsVehicleAimAngleAdjustable
         AdjustAimPower = 0x800,     // Lua_IsVehicleAimPowerAdjustable
-        Unk5 = 0x1000,
+        Flag_0X1000 = 0x1000,
     }
 }
