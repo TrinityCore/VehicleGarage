69b15d2d07b46ea18a983e2cb98392c9db38afb1
Machiavelli <machiavelli.trinity@gmail.com>
2011-06-23 02:46:52 +0200

    Add VehiclePowerTypes enum

---
+++ VehicleGarage/DBCStructures/VehiclePowerTypes.cs
@@ -0,0 +1,15 @@
+﻿
+namespace VehicleGarage.DBCStructures
+{
+    public enum VehiclePowerTypes
+    {
+        None = 0,
+        PowerType_1 = 1,
+        Pyrite = 41,
+        Steam = 61,
+        Heat = 101,
+        Ooze = 121,
+        Blood = 141,
+        Wrath = 142
+    }
+}
